ee579633474f84cc1e529d83a9a0442ed7388841
urlordjames <urlordjames@gmail.com>
2021-06-05 16:01:47 -0400

    add div for plugins and improve log.lua example

--- plugins/log.lua
+++ plugins/log.lua
@@ -1,9 +1,22 @@
 local script = [[
-ws.send(JSON.stringify({
-	"type": "plugin",
-	"id": %s,
-	"msg": window.location.href
-}));
+let mydiv = document.createElement("div");
+document.getElementById("plugin_div").appendChild(mydiv);
+
+let myinput = document.createElement("input");
+myinput.value = "some text";
+mydiv.appendChild(myinput);
+
+let mybutton = document.createElement("button");
+mybutton.innerHTML = "log";
+mydiv.appendChild(mybutton);
+
+mybutton.addEventListener("click", async function() {
+	ws.send(JSON.stringify({
+		"type": "plugin",
+		"id": %s,
+		"msg": myinput.value
+	}));
+});
 ]]
 
 local formatted_script = string.format(script, get_id());
