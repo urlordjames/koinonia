2464ac12fe79090869852682148b8040d9012434
urlordjames <urlordjames@gmail.com>
2021-06-20 14:13:50 -0400

    set upload path to a temporary directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <drogon/HttpAppFramework.h>
+#include <filesystem>
 
 int main() {
 	drogon::app().setDocumentRoot("static");
+	drogon::app().setUploadPath(std::filesystem::temp_directory_path());
 	drogon::app().addListener("0.0.0.0", 8080).run();
 }
