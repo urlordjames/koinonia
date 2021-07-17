ecc006123136635446e358fda387626cfb8c827e
urlordjames <urlordjames@gmail.com>
2021-07-16 21:54:59 -0400

    create upload directory before drogon event loop starts

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int main() {
 	drogon::app().setDocumentRoot("static");
 	std::filesystem::path upload_path = std::filesystem::temp_directory_path() / "koinonia_uploads";
+	std::filesystem::create_directories(upload_path);
 	drogon::app().setUploadPath(upload_path.string());
 	drogon::app().addListener("0.0.0.0", 8080).run();
 }
