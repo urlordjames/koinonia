9670df4f7cca2ea9a219783e8afc57ca3cd943f2
urlordjames <urlordjames@gmail.com>
2021-07-18 20:54:48 -0400

    fix Windows build

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ int main() {
 	drogon::app().setDocumentRoot("static");
 
 	std::filesystem::path upload_path = std::filesystem::temp_directory_path() / "koinonia_uploads";
-	drogon::app().setUploadPath(upload_path);
+	// explicit string conversion required, doesn't compile on Windows otherwise
+	drogon::app().setUploadPath(upload_path.string());
 
 	drogon::app().addListener("0.0.0.0", 8080).run();
 }
