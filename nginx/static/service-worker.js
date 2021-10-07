d3034e176094d709cef74d00cb4b7137f35f183b
urlordjames <32751441+urlordjames@users.noreply.github.com>
2021-10-07 17:26:14 -0400

    change serviceworker cache

--- nginx/static/service-worker.js
+++ nginx/static/service-worker.js
@@ -1,11 +1,16 @@
 // why do I need to do this myself?
+self.addEventListener("install", function(event) {
+	caches.open("koinonia-cache").then(function(cache) {
+		return cache.addAll([
+			"/",
+			"/style.css"
+			// probably not a good idea to cache koinonia.js yet
+		]);
+	});
+});
+
 self.addEventListener("fetch", function(event) {
-	event.respondWith(caches.open("koinonia-cache").then(function(cache) {
-		return cache.match(event.request).then(function(response) {
-			return response || fetch(event.request).then(function(response) {
-				cache.put(event.request, response.clone());
-				return response;
-			});
-		});
+	event.respondWith(caches.match(event.request).then(function(response) {
+		return response || fetch(event.request);
 	}));
 });
